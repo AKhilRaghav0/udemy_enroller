f9eaeae9566c6297d0918bd78796a866f91197d1
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2022-11-24 08:53:18 +0530

    Update udemy_install.sh

--- udemy_install.sh
+++ udemy_install.sh
@@ -1,7 +1,7 @@
 pkg install wget -y
 pkg install python3 -y
 pkg install git -y 
-git clone https://github.com/AKhilRaghav0/udemy-enroller/ #cloning repo
+git clone https://github.com/AKhilRaghav0/udemy_enroller/ #cloning repo
 cd udemy-enroller 
 ls
 python3 -m pip install --upgrade pip 
